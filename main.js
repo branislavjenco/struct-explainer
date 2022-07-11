ab5b8c7cbcc5013b01322ed8b2af9c7d492a448f
Branislav Jenco <branislav.jenco@sesam.io>
2022-07-11 19:13:38 +0200

    Reacting to onload, adding favicon

--- main.js
+++ main.js
@@ -3,15 +3,27 @@ const submitElement = document.querySelector("#format-submit");
 const formElement = document.querySelector("#format-form");
 const explanationElement = document.querySelector("#explanation");
 
-window.addEventListener("hashchange", (event) => {
+function main(url) {
   try {
-    const formatString = decodeURIComponent(event.newURL.split("#")[1]);
+    const formatString = decodeURIComponent(url.split("#")[1]);
     inputElement.value = formatString;
     const explanation = explainFormatString(formatString);
     const explanationToRender = renderExplanation(explanation);
     explanationElement.innerHTML = explanationToRender;
   } catch (e) {
     explanationElement.textContent = "Is that correct? " + e.message;
+  }
+}
+
+window.addEventListener("hashchange", (event) => {
+  if (event.newURL) {
+    main(event.newURL);
+  }
+});
+
+window.addEventListener("load", (event) => {
+  if (event.target.URL) {
+    main(event.target.URL);
   }
 });
 
